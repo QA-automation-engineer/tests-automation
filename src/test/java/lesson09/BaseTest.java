5dc9c9d955681f61dcda8ca6d1e2fa95e63d47c0
trainer <vladimir.trandafilov@infopulse.com>
2017-12-20 18:11:29 +0200

    feat(lesson12): add RemoteWebDriver

--- src/test/java/lesson09/BaseTest.java
+++ src/test/java/lesson09/BaseTest.java
@@ -4,8 +4,12 @@ import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.events.EventFiringWebDriver;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -14,12 +18,15 @@ public class BaseTest extends SimpleAPI {
     static WebDriver webDriver;
 
     @BeforeClass
-    public static void setUp(){
-        EventFiringWebDriver eventDriver = new EventFiringWebDriver(new ChromeDriver());
-        eventDriver.register(new EventListener());
-        webDriver = eventDriver;
+    public static void setUp() throws MalformedURLException {
+        if ("false".equals(System.getProperty("isRemote"))){
+            webDriver = new ChromeDriver();
+        } else {
+            DesiredCapabilities cap = DesiredCapabilities.chrome();
+            webDriver = new RemoteWebDriver(new URL("http://169.254.21.186:4444/wd/hub"), cap);
+        }
         //webDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        eventDriver.manage().window().maximize();
+        webDriver.manage().window().maximize();
     }
 
     @AfterClass
